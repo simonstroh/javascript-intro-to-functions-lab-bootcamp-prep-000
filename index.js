f7af2ec6f14e2beafd6d22a42583cdfd0f9c4461
Simon Stroh <simon_stroh@outlook.com>
2018-01-24 01:13:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,4 +18,8 @@ function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
     return "I can't hear you!"
   }
+  if (string === string.toUpperCase()) {
+    return "YES INDEED!"
+  }
 }
+
