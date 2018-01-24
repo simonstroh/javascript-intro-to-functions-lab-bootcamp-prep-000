bf8eb60a65584957e22d1ceba093f864f7380113
Simon Stroh <simon_stroh@outlook.com>
2018-01-24 00:56:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,8 +16,6 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string.toLowerCase()) {
-    return "I can\'t here you!"
+    return "I can't here you!"
   }
 }
-
-sayHiToGradma('hello!')
